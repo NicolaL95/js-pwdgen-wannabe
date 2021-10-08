0cb1b2df9ec5695d66a8f273c43a0ad28414c41b
NicolaL95 <nicola.lilla@gmail.com>
2021-10-08 17:31:28 +0200

    Fix

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-let str_pw = 1
-
-let user_name = prompt('inserisci il tuo nome:')
+const user_name = prompt('inserisci il tuo nome:')
 
 const last_name = prompt("inserisci il tuo cognome:")
 
@@ -8,7 +6,5 @@ const color_name = prompt("inserisci il tuo colore preferito:")
 
 
 const val_pw = user_name + last_name + color_name + "21";
-
-if (str_pw != 1) alert("La tua password non è sicura");
 
 alert("La tua password è " + val_pw);
